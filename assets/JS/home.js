2db3842c28e7cd917e2f706ac817e31d39972548
Cong Chau <congchau206#gmail.com>
2024-11-04 21:16:35 +0700

    get user name from loginForm to Home and some mini change

--- assets/JS/home.js
+++ assets/JS/home.js
@@ -1,16 +1,24 @@
+// Lay ten dang nhap
+const userName = localStorage.getItem("userName");
+
+function getUserNameFromLogin() {
+  let nameAccount = document.querySelector(".account");
+  nameAccount.innerText = userName + " | ";
+}
+
 // Button logout
 function logoutButton() {
-  var btnActive = document.querySelectorAll(".logout");
+  let btnActive = document.querySelector(".logout");
 
-  btnActive.forEach(function (button) {
-    button.addEventListener("click", function (event) {
-      var result = confirm("Bạn có chắc chắn muốn đăng xuất?");
-      if (result) {
-        window.location.href = "./index.html";
-      }
-    });
-  });
+  btnActive.onclick = function () {
+    var result = confirm("Bạn có chắc chắn muốn đăng xuất?");
+    if (result) {
+      window.location.href = "./index.html";
+      localStorage.removeItem("userName");
+    }
+  };
 }
 
 // main
+getUserNameFromLogin();
 logoutButton();
--- assets/JS/loginform.js
+++ assets/JS/loginform.js
@@ -1,40 +1,33 @@
 // Nút đăng ký của trang login || register
 function registerButton() {
-  var btnRegister = document.querySelectorAll(".register");
+  let btnRegister = document.querySelectorAll(".register");
+  if (btnRegister.length === 0) {
+    console.error("Không tìm thấy nút đăng ký.");
+    return; // Kết thúc hàm nếu không tìm thấy nút
+  }
+
   btnRegister.forEach(function (button) {
     button.addEventListener("click", function (event) {
-      // Lấy tên thẻ login-form
-      var parentClass = event.target.parentElement.className;
-      // ----btn đăng ký của form đăng nhập
+      let parentClass = event.target.parentElement.className;
+
       if (parentClass === "login-form") {
-        // Đi đến trang đăng ký
         event.target.parentElement.classList.add("hide-form");
-        var registerForm = document.querySelector(".register-form");
+        let registerForm = document.querySelector(".register-form");
         registerForm.classList.remove("hide-form");
       }
 
-      // ----btn đăng ký của form đăng ký
       if (parentClass === "register-form") {
-        // Thao tác lưu tài khoản mới đã được tạo
         // Kiểm tra tên tài khoản đã tồn tại hay chưa
         fetch("./assets/dataJSON/account.JSON")
-          .then(function (response) {
-            return response.json();
-          })
-          .then(function (data) {
-            // Kiểm tra tên tài khoản
+          .then((response) => response.json())
+          .then((data) => {
             let inputNameAcc = document.querySelector(
               ".register-form .name-account"
             );
             let nameAccountValue = inputNameAcc.value;
-            let found = false;
-            for (let i = 0; i < data.length; i++) {
-              const user = data[i];
-              if (user.nameAccount === nameAccountValue) {
-                found = true;
-                break; // Nếu tìm thấy thì thoát khỏi vòng lặp
-              }
-            }
+            let found = data.some(
+              (user) => user.nameAccount === nameAccountValue
+            );
             if (found) {
               inputNameAcc.classList.add("red-error");
               alert("Tên tài khoản đã tồn tại");
@@ -43,7 +36,6 @@ function registerButton() {
                 ".register-form .pass-account"
               );
               let password = inputPassword.value;
-
               let confirmPass = document.querySelector(".confirm-pass");
               let confirm = confirmPass.value;
 
@@ -52,35 +44,16 @@ function registerButton() {
                 confirmPass.classList.add("red-error");
                 alert("Mật khẩu xác nhận không chính xác");
               } else {
-                // Viết api gửi yêu cầu POST đến back end
+                // Xu ly luu tai khoan moi duoc tao
                 // ...
-                // fetch("Địa chỉ nhận yêu cầu", {
-                //   method: "POST",
-                //   headers: {
-                //     "Content-Type": "application/json",
-                //   },
-                //   body: JSON.stringify({ nameAccountValue, password }),
-                // })
-                //   .then(function (response) {
-                //     return response.json(); // chuyển phản hồi thành định dạng json
-                //   })
-                //   .then(function () {
-                //     alert(
-                //       "Tài khoản đã được lưu, bạn sẽ quay về trang đăng nhập"
-                //     );
-                //     event.target.parentElement.classList.add("hide-form");
-                //     var loginForm = document.querySelector(".login-form");
-                //     loginForm.classList.remove("hide-form");
-                //   });
-
                 alert("Tài khoản đã được lưu, bạn sẽ quay về trang đăng nhập");
                 event.target.parentElement.classList.add("hide-form");
-                var loginForm = document.querySelector(".login-form");
+                let loginForm = document.querySelector(".login-form");
                 loginForm.classList.remove("hide-form");
               }
             }
           })
-          .catch(function (error) {
+          .catch((error) => {
             console.error("Loading JSON:", error);
           });
       }
@@ -90,12 +63,12 @@ function registerButton() {
 
 // Nút quên mật khẩu
 function forgetPassButton() {
-  var btnActive = document.querySelectorAll(".forget-password");
+  let btnActive = document.querySelectorAll(".forget-password");
   btnActive.forEach(function (button) {
     button.addEventListener("click", function (event) {
       // Đi đến trang tìm tài khoản
       event.target.parentElement.classList.add("hide-form");
-      var getNextForm = document.querySelector(".forget-form");
+      let getNextForm = document.querySelector(".forget-form");
       getNextForm.classList.remove("hide-form");
     });
   });
@@ -103,11 +76,11 @@ function forgetPassButton() {
 
 // Nút đã có tài khoản
 function haveAccountButton() {
-  var btnActive = document.querySelectorAll(".have-account");
+  let btnActive = document.querySelectorAll(".have-account");
   btnActive.forEach(function (button) {
     button.addEventListener("click", function (event) {
       event.target.parentElement.classList.add("hide-form");
-      var getNextForm = document.querySelector(".login-form");
+      let getNextForm = document.querySelector(".login-form");
       getNextForm.classList.remove("hide-form");
     });
   });
@@ -115,14 +88,14 @@ function haveAccountButton() {
 
 // Nút Tìm tài khoản
 function findAccountButton() {
-  var btnActive = document.querySelectorAll(".find-account");
+  let btnActive = document.querySelectorAll(".find-account");
   btnActive.forEach(function (button) {
     button.addEventListener("click", function (event) {
       // Kiểm tra tên tài khoản đã nhập và đi đến trang reset mật khẩu
       // ...
 
       event.target.parentElement.classList.add("hide-form");
-      var getNextForm = document.querySelector(".resetpass-form");
+      let getNextForm = document.querySelector(".resetpass-form");
       getNextForm.classList.remove("hide-form");
     });
   });
@@ -130,7 +103,7 @@ function findAccountButton() {
 
 // Nút lưu mật khẩu
 function buttonSavePass() {
-  var btnActive = document.querySelectorAll(".save-pass");
+  let btnActive = document.querySelectorAll(".save-pass");
   btnActive.forEach(function (button) {
     button.addEventListener("click", function (event) {
       // Thao tác cập nhật lại dữ liệu thay đổi
@@ -138,7 +111,7 @@ function buttonSavePass() {
 
       alert("Mật khẩu mới của bạn đã được lưu, quay trở về trang đăng nhập");
       event.target.parentElement.classList.add("hide-form");
-      var getNextForm = document.querySelector(".login-form");
+      let getNextForm = document.querySelector(".login-form");
       getNextForm.classList.remove("hide-form");
     });
   });
@@ -146,38 +119,34 @@ function buttonSavePass() {
 
 // Nút đăng nhập
 function buttonLogin() {
-  var btnActive = document.querySelector(".login");
-  btnActive.onclick = function () {
-    // Thao tác lấy tên đăng nhập và mật khẩu để kiểm tra tính đúng sai
-    var nameAccount = document.querySelector(".name-account").value;
-    var password = document.querySelector(".pass-account").value;
+  let btnActive = document.querySelector(".login");
+  if (!btnActive) {
+    console.error("Nút đăng nhập không tồn tại.");
+    return; // Kết thúc hàm nếu không tìm thấy nút
+  }
 
-    // Nhận data
+  btnActive.onclick = function () {
+    let nameAccount = document.querySelector(".name-account").value;
+    let password = document.querySelector(".pass-account").value;
     fetch("./assets/dataJSON/account.JSON")
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        // Kiểm tra tên tài khoản
-        let found = false;
-        for (let i = 0; i < data.length; i++) {
-          const user = data[i];
-          if (user.nameAccount === nameAccount && user.password === password) {
-            found = true;
-            break; // Nếu tìm thấy thì thoát khỏi vòng lặp
-          }
-        }
+      .then((response) => response.json())
+      .then((data) => {
+        let found = data.some(
+          (user) =>
+            user.nameAccount === nameAccount && user.password === password
+        );
         if (found) {
+          localStorage.setItem("userName", nameAccount);
           window.location.href = "./home.html";
         } else {
-          var inputNameAcc = document.querySelector(".name-account");
-          var inputPass = document.querySelector(".pass-account");
+          let inputNameAcc = document.querySelector(".name-account");
+          let inputPass = document.querySelector(".pass-account");
           inputNameAcc.classList.add("red-error");
           inputPass.classList.add("red-error");
           alert("Tên đăng nhập hoặc mật khẩu sai");
         }
       })
-      .catch(function (error) {
+      .catch((error) => {
         console.error("Loading JSON:", error);
       });
   };
